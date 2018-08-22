8c6df41a9dfd224f1387adaa0c3d2a6da5708202
heyanlong <yanlong.hee@gmail.com>
2018-08-22 13:53:24 +0800

    add psr interface

--- src/psr/http_message/uploaded_file_interface.c
+++ src/psr/http_message/uploaded_file_interface.c
@@ -8,6 +8,21 @@
 
 zend_class_entry *guzzle_psr_http_message_uploaded_file_interface_ce;
 
-void guzzle_init_psr_http_message_uploaded_file_interface() {
+static zend_function_entry guzzle_psr_http_message_uploaded_file_interface_method[] = {
+        PHP_ABSTRACT_ME(UploadedFileInterface, getStream, NULL)
+        PHP_ABSTRACT_ME(UploadedFileInterface, moveTo, arginfo_psr_http_message_uploaded_file_moveTo)
+        PHP_ABSTRACT_ME(UploadedFileInterface, getSize, NULL)
+        PHP_ABSTRACT_ME(UploadedFileInterface, getError, NULL)
+        PHP_ABSTRACT_ME(UploadedFileInterface, getClientFilename, NULL)
+        PHP_ABSTRACT_ME(UploadedFileInterface, getClientMediaType, NULL)
+        PHP_FE_END
+};
 
+void guzzle_init_psr_http_message_uploaded_file_interface() {
+    zend_class_entry ce;
+
+    INIT_NS_CLASS_ENTRY(ce, PSR_HTTP_MESSAGE_NS, "UploadedFileInterface",
+                        guzzle_psr_http_message_uploaded_file_interface_method);
+
+    guzzle_psr_http_message_uploaded_file_interface_ce = zend_register_internal_interface(&ce);
 }
--- src/psr/http_message/uploaded_file_interface.h
+++ src/psr/http_message/uploaded_file_interface.h
@@ -7,4 +7,8 @@
 
 void guzzle_init_psr_http_message_uploaded_file_interface();
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_psr_http_message_uploaded_file_moveTo, ZEND_SEND_BY_VAL, ZEND_RETURN_VALUE, 0)
+                ZEND_ARG_INFO(0, targetPath)
+ZEND_END_ARG_INFO()
+
 #endif //GUZZLE_PSR_HTTP_MESSAGE_UPLOADED_FILE_INTERFACE_H
